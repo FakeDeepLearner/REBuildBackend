3804148846010b7086d3d9f66ab62d17f62ea49e
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2024-05-13 01:13:12 -0400

    Authorities
    Added free and paid authorities (just for the sake of it), and implemented UserDetails

---
+++ src/main/java/com/rebuild/backend/model/entities/Authority.java
@@ -0,0 +1,10 @@
+package com.rebuild.backend.model.entities;
+
+
+public enum Authority {
+
+    USER_FREE,
+
+    USER_PAID;
+
+}
--- src/main/java/com/rebuild/backend/model/entities/User.java
+++ src/main/java/com/rebuild/backend/model/entities/User.java
@@ -3,7 +3,11 @@ package com.rebuild.backend.model.entities;
 import com.rebuild.backend.model.entities.Resume;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,7 +20,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(
@@ -50,5 +54,43 @@ public class User {
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Resume> resumes;
 
+    @Enumerated(EnumType.STRING)
+    private Authority authority = Authority.USER_FREE;
 
+    @Transient
+    private boolean accountNonExpired = false;
+
+    @Transient
+    private boolean accountNonLocked = false;
+
+    @Transient
+    private boolean credentialsNonExpired = false;
+
+    @Transient
+    private boolean enabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(authority.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
