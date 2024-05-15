f2672647c2d35bd8f32c6170813edbc2ab4c033e
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2024-05-15 01:06:01 -0400

    Controller separation
    Separated the main form controller into separate controllers for post and out requests

---
+++ src/main/java/com/rebuild/backend/controller/resume_controllers/ResumePostController.java
@@ -0,0 +1,41 @@
+package com.rebuild.backend.controller.resume_controllers;
+
+import com.rebuild.backend.model.entities.Education;
+import com.rebuild.backend.model.entities.Experience;
+import com.rebuild.backend.model.entities.Header;
+import com.rebuild.backend.model.forms.resume_forms.EducationForm;
+import com.rebuild.backend.model.forms.resume_forms.ExperienceForm;
+import com.rebuild.backend.model.forms.resume_forms.HeaderForm;
+import com.rebuild.backend.service.ResumeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/post")
+public class ResumePostController {
+    private final ResumeService resumeService;
+
+    @Autowired
+    public ResumePostController(ResumeService resumeService) {
+        this.resumeService = resumeService;
+    }
+
+    @PostMapping("/header/{res_id}")
+    public Header createNewHeader(@PathVariable UUID res_id, @RequestBody HeaderForm headerForm){
+        return resumeService.createNewHeader(res_id, headerForm.name(), headerForm.email(), headerForm.number());
+    }
+
+    @PostMapping("/experience/{res_id}")
+    public Experience createNewExperience(@PathVariable UUID res_id, @RequestBody ExperienceForm experienceForm){
+        return resumeService.createNewExperience(res_id, experienceForm.companyName(),
+                experienceForm.timePeriod(), experienceForm.bullets());
+    }
+
+    @PostMapping("/education/{res_id}")
+    public Education createNewEducation(@PathVariable UUID res_id, @RequestBody EducationForm educationForm){
+        return resumeService.createNewEducation(res_id, educationForm.schoolName(),
+                educationForm.relevantCoursework());
+    }
+}
--- src/main/java/com/rebuild/backend/controller/resume_controllers/ResumePutController.java
+++ src/main/java/com/rebuild/backend/controller/resume_controllers/ResumePutController.java
@@ -1,4 +1,4 @@
-package com.rebuild.backend.controller;
+package com.rebuild.backend.controller.resume_controllers;
 
 import com.rebuild.backend.model.entities.Education;
 import com.rebuild.backend.model.entities.Experience;
@@ -7,47 +7,28 @@ import com.rebuild.backend.model.forms.resume_forms.EducationForm;
 import com.rebuild.backend.model.forms.resume_forms.ExperienceForm;
 import com.rebuild.backend.model.forms.resume_forms.HeaderForm;
 import com.rebuild.backend.service.ResumeService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
-public class ResumeFormController {
+@RequestMapping("/api/put")
+public class ResumePutController {
     private final ResumeService resumeService;
 
     @Autowired
-    public ResumeFormController(ResumeService resumeService) {
+    public ResumePutController(ResumeService resumeService) {
         this.resumeService = resumeService;
     }
 
-
-    @PostMapping("/api/post/header/{res_id}")
-    public Header createNewHeader(@PathVariable UUID res_id, @RequestBody HeaderForm headerForm){
-        return resumeService.createNewHeader(res_id, headerForm.name(), headerForm.email(), headerForm.number());
-    }
-
-    @PostMapping("/api/post/experience/{res_id}")
-    public Experience createNewExperience(@PathVariable UUID res_id, @RequestBody ExperienceForm experienceForm){
-        return resumeService.createNewExperience(res_id, experienceForm.companyName(),
-                experienceForm.timePeriod(), experienceForm.bullets());
-    }
-
-    @PostMapping("/api/post/education/{res_id}")
-    public Education createNewEducation(@PathVariable UUID res_id, @RequestBody EducationForm educationForm){
-        return resumeService.createNewEducation(res_id, educationForm.schoolName(),
-                educationForm.relevantCoursework());
-    }
-
-    @PutMapping("/api/put/header/{res_id}")
+    @PutMapping("/header/{res_id}")
     public Header modifyHeader(@PathVariable UUID res_id, @RequestBody HeaderForm headerForm){
         return resumeService.changeHeaderInfo(res_id, headerForm.name(),
                 headerForm.email(), headerForm.number());
     }
 
-    @PutMapping("/api/put/experience/{res_id}/{exp_id}")
+    @PutMapping("/experience/{res_id}/{exp_id}")
     public Experience modifyExperience(@PathVariable UUID res_id, @PathVariable UUID exp_id,
                                        @RequestBody ExperienceForm experienceForm){
         return resumeService.changeExperienceInfo(res_id, exp_id, experienceForm.companyName(),
@@ -55,10 +36,9 @@ public class ResumeFormController {
 
     }
 
-    @PutMapping("/api/put/education/{res_id}")
+    @PutMapping("/education/{res_id}")
     public Education modifyEducation(@PathVariable UUID res_id, @RequestBody EducationForm educationForm){
         return resumeService.changeEducationInfo(res_id, educationForm.schoolName(),
                 educationForm.relevantCoursework());
     }
-
 }
