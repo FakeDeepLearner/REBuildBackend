a09aecaa4528c489d14aa999c8d6c4b70efa3145
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2024-05-21 22:48:42 -0400

    Added proper response codes to all exceptions

--- src/main/java/com/rebuild/backend/exceptions/UserNotFoundException.java
+++ src/main/java/com/rebuild/backend/exceptions/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.rebuild.backend.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends IllegalArgumentException{
     public UserNotFoundException(String message){
         super(message);
