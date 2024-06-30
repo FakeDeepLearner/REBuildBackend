c532c20f52783e1be43ba3aec8e46e15a2629739
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2024-06-30 00:30:11 -0400

    Started redis cache
    Started to implement the cache configurations for blaclisting jwt tokens

--- src/main/java/com/rebuild/backend/config/redis/TokenStorage.java
+++ src/main/java/com/rebuild/backend/config/redis/TokenStorage.java
@@ -1,10 +1,18 @@
 package com.rebuild.backend.config.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
+import java.time.Duration;
+
 @Configuration
+@EnableCaching
 public class TokenStorage {
 
     private final RedisConnectionFactory connectionFactory;
@@ -12,6 +20,20 @@ public class TokenStorage {
     @Autowired
     public TokenStorage(RedisConnectionFactory connectionFactory) {
         this.connectionFactory = connectionFactory;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(){
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig().
+                entryTtl(Duration.ofMinutes(15)).
+                disableCachingNullValues().
+                prefixCacheNameWith("jwt_tokens::");
+
+        return RedisCacheManager.
+                builder(RedisCacheWriter.lockingRedisCacheWriter(connectionFactory)).
+                cacheDefaults(cacheConfiguration).
+                transactionAware().
+                build();
     }
 
 }
