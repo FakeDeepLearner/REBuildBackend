3587a9eb4117e416e9f912fd6ac283ab3ea7ec47
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2024-07-04 22:48:07 -0400

    Fixed small issue

--- src/main/java/com/rebuild/backend/config/security/filters/JwtBlacklistFilter.java
+++ src/main/java/com/rebuild/backend/config/security/filters/JwtBlacklistFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
-import org.hibernate.annotations.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
