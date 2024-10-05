4a94d4358f28657232bfa4d2d369b5df40b5e0f1
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2024-10-05 04:22:13 -0400

    Scrapped uploading and parsing directly from .txt files, too much work for too little benefit

--- src/main/java/com/rebuild/backend/exceptions/resume_parse_exceptions/PlaceHolder.java
+++
@@ -1,7 +0,0 @@
-package com.rebuild.backend.exceptions.resume_parse_exceptions;
-
-public class PlaceHolder extends RuntimeException {
-    public PlaceHolder(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/rebuild/backend/service/resume_services/FileParseService.java
+++
@@ -1,7 +0,0 @@
-package com.rebuild.backend.service.resume_services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FileParseService {
-}
