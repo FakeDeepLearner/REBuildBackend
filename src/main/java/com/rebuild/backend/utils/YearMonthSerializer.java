95c310ca3a7415bb22a3e9509b6194a1688b204e
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2024-12-31 00:27:56 +0300

    Create YearMonthSerializer.java
    Created a new Json Serializer for YearMonth, the type that will be used to store the year and month (because those are all that is needed at this time), of the parts of the application that need it. The actual refactoring that uses the YearMonth type will take place tomorrow

---
+++ src/main/java/com/rebuild/backend/utils/YearMonthSerializer.java
@@ -0,0 +1,43 @@
+package com.rebuild.backend.utils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.YearMonth;
+
+@Component
+public class YearMonthSerializer extends JsonSerializer<YearMonth> {
+
+    private String determineMonthString(int monthValue){
+        return switch (monthValue) {
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            case 7 -> "July";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "October";
+            case 11 -> "November";
+            case 12 -> "December";
+            default -> throw new IllegalStateException("Unexpected value: " + monthValue);
+        };
+    }
+
+    @Override
+    public void serialize(YearMonth yearMonth,
+                          JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider) throws IOException {
+
+
+        int year = yearMonth.getYear();
+        int month = yearMonth.getMonthValue();
+        String serializedResult = determineMonthString(month) + " " + year;
+        jsonGenerator.writeString(serializedResult);
+    }
+}
