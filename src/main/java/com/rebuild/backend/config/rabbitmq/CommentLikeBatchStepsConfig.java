45fd6f8c29e0984770068c73f71a44d92c9722ad
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2025-01-06 03:41:48 +0300

    Batch jobs
    Set up step and job configurations for comment and post like requests

---
+++ src/main/java/com/rebuild/backend/config/rabbitmq/CommentLikeBatchStepsConfig.java
@@ -0,0 +1,54 @@
+package com.rebuild.backend.config.rabbitmq;
+
+import com.rebuild.backend.model.entities.forum_entities.CommentLike;
+import com.rebuild.backend.model.entities.forum_entities.PostLike;
+import com.rebuild.backend.model.forms.dtos.forum_dtos.CommentLikeRequest;
+import com.rebuild.backend.model.forms.dtos.forum_dtos.PostLikeRequest;
+import com.rebuild.backend.utils.batch.processors.CommentLikeProcessor;
+import com.rebuild.backend.utils.batch.readers.RestartableCommentLikeReader;
+import com.rebuild.backend.utils.batch.writers.CommentsWriter;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+@Configuration
+public class CommentLikeBatchStepsConfig {
+
+    private final RestartableCommentLikeReader likeReader;
+
+    private final CommentsWriter commentsWriter;
+
+    private final CommentLikeProcessor likeProcessor;
+
+    @Autowired
+    public CommentLikeBatchStepsConfig(RestartableCommentLikeReader likeReader,
+                                       CommentsWriter commentsWriter,
+                                       CommentLikeProcessor likeProcessor) {
+        this.likeReader = likeReader;
+        this.commentsWriter = commentsWriter;
+        this.likeProcessor = likeProcessor;
+    }
+
+    @Bean
+    public Step commentLikeStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("commentLikeStep", jobRepository).
+                <CommentLikeRequest, CommentLike>chunk(15, transactionManager).
+                reader(likeReader).
+                processor(likeProcessor).
+                writer(commentsWriter).build();
+    }
+
+    @Bean
+    public Job commentLikeJob(JobRepository jobRepository, @Qualifier("commentLikeStep") Step postLikeStep) {
+        return new JobBuilder("commentLikeJob", jobRepository).start(postLikeStep).build();
+    }
+}
--- src/main/java/com/rebuild/backend/config/rabbitmq/PostLikeBatchStepsConfig.java
+++ src/main/java/com/rebuild/backend/config/rabbitmq/PostLikeBatchStepsConfig.java
@@ -1,18 +1,53 @@
 package com.rebuild.backend.config.rabbitmq;
 
+import com.rebuild.backend.model.entities.forum_entities.PostLike;
 import com.rebuild.backend.model.forms.dtos.forum_dtos.PostLikeRequest;
 import com.rebuild.backend.utils.batch.processors.PostLikeProcessor;
 import com.rebuild.backend.utils.batch.readers.RestartablePostLikeReader;
 import com.rebuild.backend.utils.batch.writers.PostLikesWriter;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableBatchProcessing
 public class PostLikeBatchStepsConfig {
+
+    private final RestartablePostLikeReader postLikeReader;
+
+    private final PostLikeProcessor postLikeProcessor;
+
+    private final PostLikesWriter postLikesWriter;
+
+    @Autowired
+    public PostLikeBatchStepsConfig(RestartablePostLikeReader postLikeReader,
+                                    PostLikeProcessor postLikeProcessor,
+                                    PostLikesWriter postLikesWriter) {
+        this.postLikeReader = postLikeReader;
+        this.postLikeProcessor = postLikeProcessor;
+        this.postLikesWriter = postLikesWriter;
+    }
+
+    @Bean
+    public Step postLikeStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("postLikeStep", jobRepository).
+                <PostLikeRequest, PostLike>chunk(15, transactionManager).
+                reader(postLikeReader).
+                processor(postLikeProcessor).
+                writer(postLikesWriter).build();
+    }
+
+    @Bean
+    public Job postLikeJob(JobRepository jobRepository, @Qualifier("postLikeStep") Step postLikeStep) {
+        return new JobBuilder("postLikeJob", jobRepository).start(postLikeStep).build();
+    }
 
 }
