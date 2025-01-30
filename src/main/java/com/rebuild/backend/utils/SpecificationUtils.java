badca9c6330b5b04801c09e4a8e672f3ec0500e0
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2025-01-30 02:17:50 -0500

    We are so back
    Created a new mechanism for using our comparison function to create a specification predicates with

--- src/main/java/com/rebuild/backend/utils/SpecificationUtils.java
+++ src/main/java/com/rebuild/backend/utils/SpecificationUtils.java
@@ -1,9 +1,14 @@
 package com.rebuild.backend.utils;
 
+import com.rebuild.backend.model.entities.enums.CompareCriteria;
+import com.rebuild.backend.model.entities.enums.ComparisonMethod;
 import com.rebuild.backend.model.entities.resume_entities.*;
+import com.rebuild.backend.specs.YearMonthStringOperations;
+import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 
@@ -39,6 +44,31 @@ public class SpecificationUtils {
                                                                       JoinType type,
                                                                       String propertyName) {
         return getPreComputedJoin(root, type, propertyName);
+    }
+
+
+    public static <T> Specification<T> createDateComparisonSpecification(String inputDate,
+                                                                         String dataBaseDate,
+                                                                         CompareCriteria criteria,
+                                                                         ComparisonMethod method,
+                                                                         int cutoff){
+
+        return (root, query, criteriaBuilder) -> {
+            int result = YearMonthStringOperations.compare(inputDate, dataBaseDate,
+                    criteria, method);
+            Expression<Integer> resultExpression = criteriaBuilder.literal(result);
+            //Below, we use the ge and le methods
+
+            //At least cutoff months or years ahead
+            if(method == ComparisonMethod.AFTER){
+                return criteriaBuilder.ge(resultExpression, cutoff);
+            }
+            //At least cutoff months or years behind
+            else{
+                return criteriaBuilder.le(criteriaBuilder.literal(-cutoff),
+                        resultExpression);
+            }
+        };
     }
 
 }
