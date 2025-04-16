ca734e97dae079811d35b10414388e4c1246c04d
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2025-04-15 20:17:49 -0400

    Update ResumeSectionEntry.java
    ResumeSectionEntry now keeps track of its start and end date

--- src/main/java/com/rebuild/backend/model/entities/resume_entities/ResumeSectionEntry.java
+++ src/main/java/com/rebuild/backend/model/entities/resume_entities/ResumeSectionEntry.java
@@ -1,9 +1,12 @@
 package com.rebuild.backend.model.entities.resume_entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rebuild.backend.utils.converters.database_converters.YearMonthDatabaseConverter;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import java.time.YearMonth;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,6 +35,16 @@ public class ResumeSectionEntry {
 
     @Column(nullable = false, name = "location")
     private String location;
+
+    @Column(name = "start_date")
+    @NonNull
+    @Convert(converter = YearMonthDatabaseConverter.class)
+    private YearMonth startDate;
+
+    @Column(name = "start_date")
+    @NonNull
+    @Convert(converter = YearMonthDatabaseConverter.class)
+    private YearMonth endDate;
 
     @NonNull
     @ElementCollection
