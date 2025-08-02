042c5e70f39a40955f4f99bc0e8b988e526a17fd
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2025-08-02 05:23:32 -0400

    Update Inbox.java
    Inbox now also keeps track of incoming messages

--- src/main/java/com/rebuild/backend/model/entities/users/Inbox.java
+++ src/main/java/com/rebuild/backend/model/entities/users/Inbox.java
@@ -6,6 +6,7 @@ import com.rebuild.backend.model.entities.messaging_and_friendship_entities.Mess
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
@@ -29,6 +30,7 @@ public class Inbox {
     })
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     @JsonIgnore
+    @NonNull
     private User associatedUser;
 
 
@@ -38,6 +40,13 @@ public class Inbox {
     @JoinColumn(name = "inbox_id")
     @OrderColumn(name = "insert_position")
     private List<FriendRequest> pendingRequests = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH
+    })
+    @JoinColumn(name = "inbox_id")
+    @OrderColumn(name = "insert_position")
+    private List<Message> incomingMessages = new ArrayList<>();
 
 
 }
