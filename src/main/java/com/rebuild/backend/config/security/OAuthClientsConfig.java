6b4de706b38a9faf5373d6191d2ed0f614b7d46e
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2025-08-09 03:38:26 -0400

    OAuth clients
    Configured GitHub and Google (for now, more to be added later) OAuth clients. The next step is to add a lot of code for google OAuth to work properly.

---
+++ src/main/java/com/rebuild/backend/config/security/OAuthClientsConfig.java
@@ -0,0 +1,51 @@
+package com.rebuild.backend.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+
+@Configuration
+public class OAuthClientsConfig {
+
+    @Value("${GITHUB_CLIENT_ID}")
+    private String githubClientId;
+
+    @Value("${GITHUB_CLIENT_SECRET}")
+    private String githubClientSecret;
+
+    @Value("${GOOGLE_CLIENT_ID}")
+    private String googleClientId;
+
+    @Value("${GOOGLE_CLIENT_SECRET}")
+    private String googleClientSecret;
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository()
+    {
+        ClientRegistration githubClient = ClientRegistration.withRegistrationId("github").
+                clientId(githubClientId).
+                clientSecret(githubClientSecret).
+                scope("read:user", "user:email").
+                clientName("GitHub").
+                authorizationUri("https://github.com/login/oauth/authorize").
+                tokenUri("https://github.com/login/oauth/access_token").
+                userInfoUri("https://api.github.com/user").
+                redirectUri("http://localhost:8080/login/oauth2/code/github").
+                authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE).
+                build();
+
+        ClientRegistration googleClient = ClientRegistration.withRegistrationId("google").
+                clientId(googleClientId).
+                clientSecret(googleClientSecret).
+                clientName("Google").
+                build();
+
+
+
+        return new InMemoryClientRegistrationRepository(githubClient, googleClient);
+    }
+}
