87899dcaca6498123059b5acb2309073fcb70770
Eren Aydin <eren03aydin@hotmail.com>
2025-12-19 15:08:06 -0500

    AuthenticationPrincipal
    Made a new resolver that directly resolves an @AuthenticationPrincipal to our domain User object. (Note, the performance will most likely be improved by placing the User object as a principal directly into the context when the authentication endpoints succeed)

--- src/main/java/com/rebuild/backend/config/other/ResolverRegistrationConfig.java
+++ src/main/java/com/rebuild/backend/config/other/ResolverRegistrationConfig.java
@@ -1,5 +1,7 @@
 package com.rebuild.backend.config.other;
 
+import com.rebuild.backend.utils.CustomAuthPrincipalResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -9,8 +11,16 @@ import java.util.List;
 
 @Configuration
 public class ResolverRegistrationConfig implements WebMvcConfigurer {
+
+    private final CustomAuthPrincipalResolver customAuthPrincipalResolver;
+
+    @Autowired
+    public ResolverRegistrationConfig(CustomAuthPrincipalResolver customAuthPrincipalResolver) {
+        this.customAuthPrincipalResolver = customAuthPrincipalResolver;
+    }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new AuthenticationPrincipalArgumentResolver());
+        resolvers.add(customAuthPrincipalResolver);
     }
 }
---
+++ src/main/java/com/rebuild/backend/utils/CustomAuthPrincipalResolver.java
@@ -0,0 +1,122 @@
+package com.rebuild.backend.utils;
+
+import com.rebuild.backend.model.entities.users.User;
+import com.rebuild.backend.service.user_services.UserService;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.annotation.MergedAnnotations;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.SecurityAnnotationScanner;
+import org.springframework.security.core.annotation.SecurityAnnotationScanners;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.lang.annotation.Annotation;
+import java.util.Optional;
+
+@Component
+public class CustomAuthPrincipalResolver implements HandlerMethodArgumentResolver {
+
+    private final Class<AuthenticationPrincipal> annotationType = AuthenticationPrincipal.class;
+    private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
+    private final ExpressionParser parser = new SpelExpressionParser();
+    private final SecurityAnnotationScanner<@NonNull AuthenticationPrincipal> scanner = SecurityAnnotationScanners.requireUnique(AuthenticationPrincipal.class);
+
+    private final UserService userService;
+
+    @Autowired
+    public CustomAuthPrincipalResolver(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return findMethodAnnotation(parameter) != null;
+    }
+
+    @Override
+    public @Nullable Object resolveArgument(MethodParameter parameter,
+                                            @Nullable ModelAndViewContainer mavContainer,
+                                            NativeWebRequest webRequest,
+                                            @Nullable WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = this.securityContextHolderStrategy.getContext().getAuthentication();
+        if (authentication == null) {
+            return null;
+        } else {
+            //The principal is a type of UserDetails, when the DaoAuthenticationProvider is used.
+            // For other potential providers, we will make sure that this is the case if it isn't already.
+            Object principal = authentication.getPrincipal();
+            AuthenticationPrincipal annotation = this.findMethodAnnotation(parameter);
+            Assert.notNull(annotation, "@AuthenticationPrincipal is required. Call supportsParameter first.");
+
+            //principal.getClass is UserDetails.
+            // parameter.getParameterType is User.
+
+            /*
+             * In the normal implementation of the resolver, a check is made that a type of UserDetails (rhs)
+             * is assignable to a type of User (lhs). This check will fail for our purposes.
+             * What we need is checking that a type of User (rhs) being assignable to a type of UserDetails (lhs).
+             * This will succeed, since UserDetails is a superinterface of User
+             */
+            if (principal != null &&
+                    !ClassUtils.isAssignable(principal.getClass(), parameter.getParameterType())) {
+                return null;
+            } else {
+
+                if (principal instanceof UserDetails details)
+                {
+                    String principalUsername = details.getUsername();
+
+                    Optional<User> foundUser = userService.findByEmailOrPhone(principalUsername);
+
+                    if  (foundUser.isPresent()) {
+                        return foundUser.get();
+                    }
+                }
+                return null;
+            }
+        }
+    }
+
+    private @Nullable AuthenticationPrincipal findMethodAnnotation(MethodParameter parameter) {
+        boolean useAnnotationTemplate = false;
+        if (useAnnotationTemplate) {
+            return this.scanner.scan(parameter.getParameter());
+        } else {
+            AuthenticationPrincipal annotation = parameter.getParameterAnnotation(this.annotationType);
+            if (annotation != null) {
+                return annotation;
+            } else {
+                Annotation[] annotationsToSearch = parameter.getParameterAnnotations();
+
+                for(Annotation toSearch : annotationsToSearch) {
+                    annotation = AnnotationUtils.findAnnotation(toSearch.annotationType(), this.annotationType);
+                    if (annotation != null) {
+                        return MergedAnnotations.from(new Annotation[]{toSearch}).get(this.annotationType).synthesize();
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
