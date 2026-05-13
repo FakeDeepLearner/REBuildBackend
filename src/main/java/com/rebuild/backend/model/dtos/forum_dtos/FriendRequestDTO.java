f8689d0baa5bda2348472e0979500a71e568645d
Eren Aydin <eren03aydin@hotmail.com>
2026-05-13 05:13:37 -0400

    Update FriendRequestDTO.java

--- src/main/java/com/rebuild/backend/model/dtos/forum_dtos/FriendRequestDTO.java
+++ src/main/java/com/rebuild/backend/model/dtos/forum_dtos/FriendRequestDTO.java
@@ -3,5 +3,7 @@ package com.rebuild.backend.model.dtos.forum_dtos;
 import java.time.Instant;
 import java.util.UUID;
 
+//Here so I don't accidentally delete this class
+@SuppressWarnings("unused")
 public record FriendRequestDTO(UUID requestId, String requestingUsername, Instant requestedAt) {
 }
