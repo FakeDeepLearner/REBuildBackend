a4621d21c9ca2c4a41039be3a4c08c1cd5c79533
Eren Aydin <eren03aydin@hotmail.com>
2026-06-28 04:53:57 -0400

    More cleanup
    Removed some now useless libraries from the class path, and removed some @Enable* annotations from the main app class

--- src/main/java/com/rebuild/backend/BackendApplication.java
+++ src/main/java/com/rebuild/backend/BackendApplication.java
@@ -1,14 +1,8 @@
 package com.rebuild.backend;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.EnableJdbcJobRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,15 +10,9 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 
 
 @SpringBootApplication
-@EnableCaching
 @EnableWebSecurity
 @EnableMethodSecurity
-@EnableScheduling
 @EnableJpaRepositories
-@EnableJpaAuditing
-@EnableBatchProcessing
-@EnableJdbcJobRepository
-@EnableRabbit
 @EnableTransactionManagement
 @EnableWebSocketMessageBroker
 public class BackendApplication {
