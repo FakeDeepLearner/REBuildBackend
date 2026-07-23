018586c59f4ffb90a746d09959006c77dc2327f0
Eren Aydin <eren03aydin@hotmail.com>
2026-07-23 02:37:58 -0400

    Update SecureAuthConfig.java
    Removed some extraneous filters and made the filter chain much easier to reason about.
    
    Created separate security chains for api and webhook endpoints, and abstracted away all of the common configurations between them into a single method.
    
    More clearly specified that the max age of the preflight request is 1 hour.

--- src/main/java/com/rebuild/backend/config/security/SecureAuthConfig.java
+++ src/main/java/com/rebuild/backend/config/security/SecureAuthConfig.java
@@ -2,27 +2,21 @@ package com.rebuild.backend.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.*;
 import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
-import org.springframework.web.filter.UrlHandlerFilter;
 
+import java.time.Duration;
 import java.util.List;
 
 @Configuration
@@ -36,8 +30,7 @@ public class SecureAuthConfig {
         corsConfiguration.setAllowedOrigins(List.of("localhost", "rerebuild.ca"));
         corsConfiguration.setAllowedHeaders(List.of("Idempotency-Key"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS",  "PATCH", "HEAD"));
-        corsConfiguration.setAllowCredentials(false);
-        corsConfiguration.setMaxAge(3600L);
+        corsConfiguration.setMaxAge(Duration.ofHours(1));
 
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
@@ -54,45 +47,49 @@ public class SecureAuthConfig {
         return repository;
     }
 
-    //This will redirect to the original endpoint if there is a trailing slash at the end of the request's URL.
-    @Bean
-    public UrlHandlerFilter handlerFilter()
-    {
-        return UrlHandlerFilter
-                .trailingSlashHandler("/api/**")
-                .redirect(HttpStatus.PERMANENT_REDIRECT)
-                .build();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChainAuthentication(HttpSecurity security,
-                                                         ClerkAuthenticationFilter authenticationFilter,
-                                                         UrlHandlerFilter urlHandlerFilter,
-                                                         CsrfTokenRepository csrfTokenRepository,
-                                                         CorsConfigurationSource corsConfigurationSource) {
+    private void commonChain(HttpSecurity security) {
         security
                 .formLogin(FormLoginConfigurer::disable)
                 .httpBasic(HttpBasicConfigurer::disable)
                 .logout(LogoutConfigurer::disable)
-                .sessionManagement(management ->
-                        management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .requestCache(RequestCacheConfigurer::disable)
                 .oauth2Login(OAuth2LoginConfigurer::disable)
-                .addFilterBefore(urlHandlerFilter, SecurityContextHolderFilter.class)
-                .addFilterAfter(new HiddenHttpMethodFilter(), UrlHandlerFilter.class)
-                .addFilterAfter(authenticationFilter, HiddenHttpMethodFilter.class)
+                .sessionManagement(management ->
+                        management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .redirectToHttps(Customizer.withDefaults());
+    }
+
+    @Bean
+    public SecurityFilterChain webhooksFilterChain(HttpSecurity security)  {
+        commonChain(security);
+        security.
+                securityMatcher("/webhooks/**")
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll());
+        return security.build();
+    }
+
+    @Bean
+    public SecurityFilterChain apiRequestsFilterChain(HttpSecurity security,
+                                                      ClerkAuthenticationFilter authenticationFilter,
+                                                      CsrfTokenRepository csrfTokenRepository,
+                                                      CorsConfigurationSource corsConfigurationSource) {
+        commonChain(security);
+        
+        security
+                .securityMatcher("/api/**")
+                .addFilterAfter(authenticationFilter, SecurityContextHolderFilter.class)
                 .csrf(management ->
                         management.csrfTokenRepository(csrfTokenRepository))
                 .headers(
                 headers -> headers.contentSecurityPolicy(
-                        csp -> csp.policyDirectives("default-src 'self'; script-src 'self'; " +
+                        csp -> csp.policyDirectives("default-src 'self'; " +
+                                "script-src 'self'; " +
                                 "img-src 'self'; frame-ancestors 'none';")
                 )).
                 authorizeHttpRequests(auth -> auth
-                .requestMatchers("/webhooks/**").permitAll()
-                .requestMatchers("/api/**").authenticated()
-                .anyRequest().denyAll())
-                .redirectToHttps(Customizer.withDefaults())
+                .anyRequest().authenticated())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource));
         return security.build();
 
